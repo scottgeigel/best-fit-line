5fca536dca75bba6c34c81e52e96ba54d475cefc
Scott Geigel <scott.geigel@gmail.com>
2018-08-19 17:35:33 -0500

    added code to check accuracy of calculated values renamed functions to match lg_line_functionname

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <math.h>
 
 struct point 
 {
@@ -13,7 +14,7 @@ typedef struct
     double intercept;
 } lg_line_t;
 
-void lg_build_line(lg_line_t* this, int count, struct point* points)
+void lg_line_build(lg_line_t* this, int count, struct point* points)
 {
     double sum_x=0, sum_xx=0, sum_xy=0;
     double sum_y=0, sum_yy=0;
@@ -31,6 +32,16 @@ void lg_build_line(lg_line_t* this, int count, struct point* points)
     this->intercept = ( ( 1.0 / (double) count ) * sum_y ) - ( this->slope * ( 1.0 / (double) count ) * sum_x );
 }
 
+double lg_line_plot(lg_line_t* this, double x)
+{
+    return ( this->slope * x ) + this->intercept;
+}
+
+double calc_error(double expected, double actual)
+{
+    return fabs( (actual - expected) / expected);
+}
+
 int main ()
 {
     struct point test_points[] = 
@@ -43,7 +54,13 @@ int main ()
 
     lg_line_t test_line;
 
-    lg_build_line(&test_line, sizeof(test_points) / sizeof(test_points[0]), test_points);
+    lg_line_build(&test_line, sizeof(test_points) / sizeof(test_points[0]), test_points);
 
     printf("line is y = %f * x + %f\n", test_line.slope, test_line.intercept);
+
+    for ( int i = 0; i < ( sizeof(test_points) / sizeof(test_points[0]) ); i++ )
+    {
+        double plotted_y = lg_line_plot(&test_line, test_points[i].x);
+        printf("checking point (%f, %f)\t\t%f\t\t%%%f\n", test_points[i].x, test_points[i].y, plotted_y, calc_error(test_points[i].y, plotted_y));
+    }
 }
